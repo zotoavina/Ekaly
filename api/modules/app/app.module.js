01570eb890ee5c34fb479be29b234d54ff61c3e8
Zotoavina <Zotoavina.RASOAHARISOA@pulse.mg>
2022-03-29 11:53:38 +0200

    initialisation node

--- api/modules/app/app.module.js
+++ api/modules/app/app.module.js
@@ -8,4 +8,4 @@ app.use(express.json());
 app.use(express.urlencoded( { extended : false }));
 app.use( cors() );
 
-module .expots = { app };
+module.exports = { app };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {main , app} = require('./api/main');
 const path= require('path');
 
+
 app.get('/app/*', (req, res) => res.sendFile(path.join(__dirname, 'dist/ekaly-front/index.html')));
 app.get('/app/', (req, res) => res.sendFile(path.join(__dirname, 'dist/ekaly-front/index.html')));
 
