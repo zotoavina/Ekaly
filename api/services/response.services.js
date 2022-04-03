9e83161dd9a91fbdd0cfdf1c75b81c0417c3ca8d
Zotoavina <Zotoavina.RASOAHARISOA@pulse.mg>
2022-04-03 16:09:05 +0200

    correction response service

--- api/services/response.services.js
+++ api/services/response.services.js
@@ -1,12 +1,12 @@
 module.exports = {
-  success = function(res, data,message){
+  success : function(res, data,message){
     res.json( {
       code : 202,
       data : data,
       message : message
     } )
   },
-  error = function(res, message){
+  error : function(res, message){
     res.json( {
       code : 400,
       message : message
